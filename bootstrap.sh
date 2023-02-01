978b77b76f629ab14c7d502169cefa52ccb4b4be
Libertea <me@libertea.xyz>
2023-02-01 09:41:15 +0000

    uninstall

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,40 +15,41 @@ REPO_URL="https://github.com/VZiChoushaDui/Libertea.git"
 PROJECT_NAME="libertea"
 COMMAND="$1"
 
-if [ "$COMMAND" != "install" ] && [ "$COMMAND" != "update" ] && [ "$COMMAND" != "install-proxy" ]; then
-    echo "Usage: $0 [install|update|install-proxy]"
+if [ "$COMMAND" != "install" ] && [ "$COMMAND" != "update" ] && [ "$COMMAND" != "install-proxy" ] && [ "$COMMAND" != "uninstall" ]; then
+    echo "Usage: $0 [install|update|install-proxy|uninstall]"
     exit 1
 fi
 
-# Make sure apt-get exists
-if ! command -v apt-get &> /dev/null; then
-    echo "apt-get not found. This script is intended for Debian-based systems only."
-    exit 1
-fi
+if [ "$COMMAND" != "uninstall" ]; then
+    # Make sure apt-get exists
+    if ! command -v apt-get &> /dev/null; then
+        echo "apt-get not found. This script is intended for Debian-based systems only."
+        exit 1
+    fi
 
-# Make sure git is installed
-if ! command -v git &> /dev/null; then
-    echo " ** Installing git..."
-    apt-get update >/dev/null
-    apt-get install -y git >/dev/null
-fi
+    # Make sure git is installed
+    if ! command -v git &> /dev/null; then
+        echo " ** Installing git..."
+        apt-get update >/dev/null
+        apt-get install -y git >/dev/null
+    fi
 
-# Clone the repository to /root if not exists, otherwise update it
-if [ -d "/root/$PROJECT_NAME" ]; then
-    echo " ** Updating repository..."
-    cd "/root/$PROJECT_NAME"
-    git reset --hard >/dev/null
-    git checkout master >/dev/null
-    git reset --hard >/dev/null
-    git clean -fd >/dev/null
-    
-    git pull --rebase >/dev/null
-else
-    echo " ** Cloning repository..."
-    git clone "$REPO_URL" "/root/$PROJECT_NAME" >/dev/null
-    cd "/root/$PROJECT_NAME"
+    # Clone the repository to /root if not exists, otherwise update it
+    if [ -d "/root/$PROJECT_NAME" ]; then
+        echo " ** Updating repository..."
+        cd "/root/$PROJECT_NAME"
+        git reset --hard >/dev/null
+        git checkout master >/dev/null
+        git reset --hard >/dev/null
+        git clean -fd >/dev/null
+        
+        git pull --rebase >/dev/null
+    else
+        echo " ** Cloning repository..."
+        git clone "$REPO_URL" "/root/$PROJECT_NAME" >/dev/null
+        cd "/root/$PROJECT_NAME"
+    fi
 fi
-
 
 if [ "$COMMAND" = "install" ]; then
     # Install the project
@@ -62,4 +63,38 @@ elif [ "$COMMAND" = "install-proxy" ]; then
     # Install the proxy
     echo "Installing $PROJECT_NAME-proxy..."
     ./init-proxy.sh "$2" "$3" "$4"
+elif [ "$COMMAND" = "uninstall" ]; then
+    set +e
+
+    echo "Are you sure you want to uninstall $PROJECT_NAME? This action is irreversible. [y/N]"
+    read -r CONFIRM
+    if [ "$CONFIRM" != "y" ] && [ "$CONFIRM" != "Y" ]; then
+        echo "Uninstall cancelled."
+        exit 0
+    fi
+
+    cd "/root/$PROJECT_NAME"
+    echo " ** Stopping docker containers..."
+    for container_name in $(docker ps -a | grep "libertea-" | awk '{print $NF}'); do
+        echo "   - $container_name"
+        docker rm -f "$container_name" >/dev/null
+    done
+    
+
+    echo " ** Stopping systemd service..."
+    pkill -9 -f uwsgi
+    systemctl kill libertea-panel.service
+    pkill -9 -f uwsgi
+    systemctl stop libertea-panel.service
+
+    echo " ** Removing systemd service..."
+    systemctl disable libertea-panel.service
+    rm -f /etc/systemd/system/libertea-panel.service
+
+    echo " ** Deleting Libertea files..."
+    cd /root
+    rm -rf "/root/$PROJECT_NAME"
+
+    echo ""
+    echo "Uninstall complete."
 fi
