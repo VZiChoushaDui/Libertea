59f2e078aa0e61b4144c1af0cc975b35a09c0d1d
Libertea <me@libertea.xyz>
2023-03-22 15:29:30 +0000

    fix uwsgi listen queue

---
+++ panel/serve.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+LOG_MAX_SIZE_MB=100
+UWSGI_THREADS=6
+PORT=1000
+
+function log {
+    cur_date=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "[$cur_date] $1"
+    echo "[$cur_date] $1" >> /tmp/libertea-panel.log
+    echo "[$cur_date] $1" >> /tmp/libertea-panel.init.log
+}
+
+log "Libertea panel serve script"
+
+dir=$(pwd)
+log "Current directory: $dir"
+
+# check if /tmp/libertea-panel.log is bigger than LOG_MAX_SIZE_MB, if so, remove it
+if [ -f /tmp/libertea-panel.log ]; then
+    log_size=$(du -m /tmp/libertea-panel.log | cut -f1)
+    if [ $log_size -gt $LOG_MAX_SIZE_MB ]; then
+        log "Removing /tmp/libertea-panel.log"
+        rm /tmp/libertea-panel.log
+        log "Removed /tmp/libertea-panel.log because it was bigger than $LOG_MAX_SIZE_MB MB"
+    fi
+fi
+
+log "Checking net.core.somaxconn value" 
+SOMAXCONN=100
+SOMAXCONN=$(sysctl -n net.core.somaxconn)
+log "  net.core.somaxconn value is $SOMAXCONN"
+if ! [[ $SOMAXCONN =~ ^[0-9]+$ ]]; then
+    SOMAXCONN=100
+fi
+if [ $SOMAXCONN -gt 512 ]; then
+    SOMAXCONN=512
+else
+    SOMAXCONN=$(($SOMAXCONN - 1))
+fi
+
+log "Starting Libertea panel via uwsgi on port $PORT with $UWSGI_THREADS threads, and listen queue size of $SOMAXCONN"
+uwsgi --disable-logging --master -p $UWSGI_THREADS --need-app --http 127.0.0.1:$PORT --listen $SOMAXCONN -w serve:app >> /tmp/libertea-panel.log 2>&1
+
+log "Libertea panel serve script exited with code $?"
