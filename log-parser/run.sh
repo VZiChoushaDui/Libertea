6a081530f2205a279f4ef604b3de4c792242a055
Libertea <me@libertea.xyz>
2023-04-08 13:43:53 +0000

    log parser

--- log-parser/run.sh
+++ log-parser/run.sh
@@ -9,12 +9,14 @@ while true; do
     files_to_compress=`ls -t | grep -v gz |  tail -n +2 | wc -l`
 
     if [ $files_to_compress -gt 0 ]; then
+        echo "Compressing $files_to_compress files"
         ls -t | grep -v gz |  tail -n +2 | xargs gzip --verbose
     fi
 
     files_to_delete=`ls -t | grep gz |  tail -n +72 | wc -l`
     if [ $files_to_delete -gt 0 ]; then
-        ls -t | grep gz |  tail -n +72 | xargs rm --verbose
+        echo "Deleting $files_to_delete old files"
+        ls -t | grep gz |  tail -n +72 | xargs rm
     fi
 
     sleep 300
