4bfe3f0709863a56192eda4b2f6c636fc3402911
Libertea <me@libertea.xyz>
2023-04-18 13:08:14 +0000

    x

--- push-docker.sh
+++ push-docker.sh
@@ -1,72 +1,67 @@
 #!/bin/bash
 
+set -e
+
 version=`cat version.txt`
 
 # build {image} and push it to libertea:{image}:version on docker hub
 
 # ./haproxy
 echo " *** Building and pushing libertea/haproxy:$version *** "
-docker build -t libertea/haproxy:$version ./haproxy
-docker push libertea/haproxy:$version
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/haproxy:$version ./haproxy --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/haproxy:latest ./haproxy --push
 
 # ./syslog
 echo " *** Building and pushing libertea/syslog:$version *** "
-docker build -t libertea/syslog:$version ./syslog
-docker push libertea/syslog:$version
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/syslog:$version ./syslog --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/syslog:latest ./syslog --push
 
 # ./log-parser
 echo " *** Building and pushing libertea/log-parser:$version *** "
-docker build -t libertea/log-parser:$version ./log-parser
-docker push libertea/log-parser:$version
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/log-parser:$version ./log-parser --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/log-parser:latest ./log-parser --push
 
 # providers/trojan-ws
 echo " *** Building and pushing libertea/provider-trojan-ws:$version *** "
-docker build -t libertea/provider-trojan-ws:$version ./providers/trojan-ws
-docker push libertea/provider-trojan-ws:$version
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-trojan-ws:$version ./providers/trojan-ws --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-trojan-ws:latest ./providers/trojan-ws --push
+
+# providers/trojan-grpc
+echo " *** Building and pushing libertea/provider-trojan-grpc:$version *** "
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-trojan-grpc:$version ./providers/trojan-grpc --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-trojan-grpc:latest ./providers/trojan-grpc --push
 
 # providers/vless-ws
 echo " *** Building and pushing libertea/provider-vless-ws:$version *** "
-docker build -t libertea/provider-vless-ws:$version ./providers/vless-ws
-docker push libertea/provider-vless-ws:$version
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-vless-ws:$version ./providers/vless-ws --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-vless-ws:latest ./providers/vless-ws --push
+
+# providers/vless-grpc
+echo " *** Building and pushing libertea/provider-vless-grpc:$version *** "
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-vless-grpc:$version ./providers/vless-grpc --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-vless-grpc:latest ./providers/vless-grpc --push
+
+# providers/vmess-grpc
+echo " *** Building and pushing libertea/provider-vmess-grpc:$version *** "
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-vmess-grpc:$version ./providers/vmess-grpc --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-vmess-grpc:latest ./providers/vmess-grpc --push
 
 # providers/shadowsocks-v2ray
 echo " *** Building and pushing libertea/provider-shadowsocks-v2ray:$version *** "
-docker build -t libertea/provider-shadowsocks-v2ray:$version ./providers/shadowsocks-v2ray
-docker push libertea/provider-shadowsocks-v2ray:$version
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-shadowsocks-v2ray:$version ./providers/shadowsocks-v2ray --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/provider-shadowsocks-v2ray:latest ./providers/shadowsocks-v2ray --push
 
 # ./proxy-register
 echo " *** Building and pushing libertea/proxy-register:$version *** "
-docker build -t libertea/proxy-register:$version ./proxy-register
-docker push libertea/proxy-register:$version
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/proxy-register:$version ./proxy-register --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/proxy-register:latest ./proxy-register --push
 
 # ./proxy-haproxy
 echo " *** Building and pushing libertea/proxy-haproxy:$version *** "
-docker build -t libertea/proxy-haproxy:$version ./proxy-haproxy
-docker push libertea/proxy-haproxy:$version
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/proxy-haproxy:$version ./proxy-haproxy --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/proxy-haproxy:latest ./proxy-haproxy --push
 
 # ./proxy-fake-traffic
 echo " *** Building and pushing libertea/proxy-fake-traffic:$version *** "
-docker build -t libertea/proxy-fake-traffic:$version ./proxy-fake-traffic
-docker push libertea/proxy-fake-traffic:$version
-
-# add latest tag to all images
-docker tag libertea/haproxy:$version libertea/haproxy:latest
-docker tag libertea/syslog:$version libertea/syslog:latest
-docker tag libertea/log-parser:$version libertea/log-parser:latest
-docker tag libertea/provider-trojan-ws:$version libertea/provider-trojan-ws:latest
-docker tag libertea/provider-vless-ws:$version libertea/provider-vless-ws:latest
-docker tag libertea/provider-shadowsocks-v2ray:$version libertea/provider-shadowsocks-v2ray:latest
-docker tag libertea/proxy-register:$version libertea/proxy-register:latest
-docker tag libertea/proxy-haproxy:$version libertea/proxy-haproxy:latest
-docker tag libertea/proxy-fake-traffic:$version libertea/proxy-fake-traffic:latest
-
-docker push libertea/haproxy:latest
-docker push libertea/syslog:latest
-docker push libertea/log-parser:latest
-docker push libertea/provider-trojan-ws:latest
-docker push libertea/provider-vless-ws:latest
-docker push libertea/provider-shadowsocks-v2ray:latest
-docker push libertea/proxy-register:latest
-docker push libertea/proxy-haproxy:latest
-docker push libertea/proxy-fake-traffic:latest
-
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/proxy-fake-traffic:$version ./proxy-fake-traffic --push
+docker buildx build --platform linux/amd64,linux/arm64 -t libertea/proxy-fake-traffic:latest ./proxy-fake-traffic --push
