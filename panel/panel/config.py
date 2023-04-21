aee71e71d8457d6f6d32a9b62158b76894776762
Libertea <me@libertea.xyz>
2023-04-21 13:53:37 +0000

    update proxy version

--- panel/panel/config.py
+++ panel/panel/config.py
@@ -18,7 +18,7 @@ def new_getaddrinfo(*args, **kwargs):
 socket.getaddrinfo = new_getaddrinfo
 
 LIBERTEA_VERSION = 1030
-LIBERTEA_PROXY_VERSION = 1002
+LIBERTEA_PROXY_VERSION = 1003
 VERSION_ENDPOINT = "https://raw.githubusercontent.com/VZiChoushaDui/Libertea/master/version.txt"
 
 HAPROXY_CONTAINER_NAME = 'libertea-haproxy'
--- proxy-register/main.py
+++ proxy-register/main.py
@@ -6,7 +6,7 @@ import socket
 import requests
 from datetime import datetime
 
-LIBERTEA_PROXY_VERSION = 1002
+LIBERTEA_PROXY_VERSION = 1003
 
 # Force ipv4 for requests
 old_getaddrinfo = socket.getaddrinfo
