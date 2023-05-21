3c1d40c04adee41b1532a5effc880868617f0def
Libertea <me@libertea.xyz>
2023-05-21 11:37:02 +0000

    x

--- init-proxy.sh
+++ init-proxy.sh
@@ -137,8 +137,7 @@ else
     systemctl restart libertea-proxy-fake-traffic.service
 
     echo "    - proxy-ssh-tunnel"
-    cd proxy-ssh-tunnel
-    ./install-services.sh "$CONN_PROXY_IP" "443" "root" # TODO: Replace user with non-root user
+    ./proxy-ssh-tunnel/install-services.sh "$CONN_PROXY_IP" "443" "root" # TODO: Replace user with non-root user
 
     echo "     - haproxy"
     systemctl stop haproxy
