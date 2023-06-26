b335de65841ab3f44b1cf2b524eaf0412ce33c9c
Libertea <me@libertea.xyz>
2023-06-26 13:25:01 +0000

    x

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+date
+
 DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd "$DIR"
 
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 
-if [[ $BRANCH != "master" ]]; then
-    echo "Not on master branch. Will not check for updates."
+# only work on 'master' and 'beta' branches
+if [[ $BRANCH != "master" ]] && [[ $BRANCH != "beta" ]]; then
+    echo "Not on master or beta branch, aborting."
     exit
 fi
 
