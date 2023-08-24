fcdc9f5170a52ed09710dd8f6d332ec41754122e
quiniapiezoelectricity <73748843+quiniapiezoelectricity@users.noreply.github.com>
2023-08-24 18:22:39 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -37,18 +37,12 @@ fi
 cd "$DIR"
 
 # if .libertea.proxy file exists, then this is a proxy server. don't install main
-if [[ -f .libertea.proxy || -f .libertea-marron.proxy ]]; then
+if [ -f .libertea.proxy ]; then
     echo "This is a Libertea proxy server. You can't install both main and proxy on the same server."
     exit 1
 fi
 
-if [[ -f .libertea.main && ( ! -f .libertea-marron.main ) ]]; then
-    echo "A vanilla version of Libertea main exists on this server. You can't install both vanilla and marron on the same server."
-    exit 1
-fi
-
 touch .libertea.main
-touch .libertea-marron.main
 export DEBIAN_FRONTEND=noninteractive
 
 echo " ** Installing dependencies..."
